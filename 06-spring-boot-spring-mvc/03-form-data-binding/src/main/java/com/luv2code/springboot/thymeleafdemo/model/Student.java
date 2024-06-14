f3b6f850b58b2235ec0cceb9298f5d4e24124cc2
Fabio Etiope <fabio.etiope@gmail.com>
2024-06-14 16:14:54 +0100

    form-data-binding

--- 06-spring-boot-spring-mvc/03-form-data-binding/src/main/java/com/luv2code/springboot/thymeleafdemo/model/Student.java
+++ 06-spring-boot-spring-mvc/03-form-data-binding/src/main/java/com/luv2code/springboot/thymeleafdemo/model/Student.java
@@ -1,8 +1,29 @@
 package com.luv2code.springboot.thymeleafdemo.model;
 
+import java.util.List;
+
 public class Student {
-    String firstName;
-    String lastName;
+    private String firstName;
+    private String lastName;
+    private String country;
+    private String favoriteLanguage;
+    private List<String> favoriteOs;
+
+    public List<String> getFavoriteOs() {
+        return favoriteOs;
+    }
+
+    public void setFavoriteOs(List<String> favoriteOs) {
+        this.favoriteOs = favoriteOs;
+    }
+
+    public String getFavoriteLanguage() {
+        return favoriteLanguage;
+    }
+
+    public void setFavoriteLanguage(String favoriteLanguage) {
+        this.favoriteLanguage = favoriteLanguage;
+    }
 
     public Student() {
     }
@@ -21,5 +42,13 @@ public class Student {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
     }
 }
